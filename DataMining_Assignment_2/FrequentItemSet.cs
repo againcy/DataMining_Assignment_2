477cff3355c2847c7c752e8cd833de2ab13f48f7
againcy <againde@gmail.com>
2015-10-21 14:02:11 +0900

    DataMining Assignment_2
    hash树 求频繁项

---
+++ DataMining_Assignment_2/FrequentItemSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataMining_Assignment_2
+{
+    class FrequentItemSet<T>
+    {
+        /// <summary>
+        /// 频繁项中的数量(frequent k-itemset)
+        /// </summary>
+        public int ItemCount
+        {
+            get
+            {
+                return itemCount;
+            }
+        }
+        private int itemCount;
+
+        private LinkedList<ItemSet<T>> itemSets;
+
+       
+        public FrequentItemSet(int k)
+        {
+            itemCount = k;
+        }
+
+
+    }
+}
---
+++ DataMining_Assignment_2/Transaction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataMining_Assignment_2
+{
+    class Transaction<T>
+    {
+        /// <summary>
+        /// 项集
+        /// </summary>
+        public LinkedList<ItemSet<T>> ItemSets
+        {
+            get
+            {
+                return itemSets;
+            }
+        }
+        private LinkedList<ItemSet<T>> itemSets;
+
+        private T[] itemName;
+        private int itemCount;
+
+        public Transaction()
+        {
+            itemSets = new LinkedList<ItemSet<T>>();
+        }
+
+        /// <summary>
+        /// 设置item名称及数量
+        /// </summary>
+        /// <param name="n">每个item set中的item数</param>
+        /// <param name="names">item的名字</param>
+        public void SetItemName(int n, T[] names)
+        {
+            itemCount = n;
+            itemName = names;
+        }
+
+        /// <summary>
+        /// 添加一个item set
+        /// </summary>
+        /// <param name="value">item set的值，0表示不存在对应的item，1表示存在</param>
+        public void AddItemSet(int[] value)
+        {
+            ItemSet<T> newItemSet = new ItemSet<T>();
+            for (int i = 0; i < itemCount; i++)
+            {
+                if (value[i] == 1) newItemSet.AddItem(itemName[i]);
+            }
+            itemSets.AddLast(newItemSet);
+        }
+
+    }
+}
